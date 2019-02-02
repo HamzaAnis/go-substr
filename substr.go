709e46fabbfa56489e684bc7cbd6e557304c5354
hamzaanis <hamzaanis9514@gmail.com>
2019-02-02 13:28:45 +0500

    Few test cases

--- substr.go
+++ substr.go
@@ -1,14 +1,19 @@
 package substr
 
-import "fmt"
-
 // This struct holds the value.
 type String struct {
 	data string
 }
 
 // Substr clone of https://help.adobe.com/en_US/FlashPlatform/reference/actionscript/3/String.html#substr()
-func (v *String) Substr(n int) string {
-	fmt.Println(v.data)
-	return "OK"
+func (v *String) Substr(n1 int, n2 ...int) string {
+	if len(n2) <= 0 {
+		if n1 >= 0 && n1 <= len(v.data) {
+			return v.data[n1:]
+		} else {
+			return ""
+		}
+	} else {
+		return v.data[n1 : n1+n2[0]]
+	}
 }
--- substr_test.go
+++ substr_test.go
@@ -5,19 +5,42 @@ import "testing"
 func TestSubstr(t *testing.T) {
 	tests := []struct {
 		in       string
-		number 	int
+		number   int
 		expected string
 	}{
-		{"OK",0, "OK"},
-		{"OK", 10,"OK"},
-		{"OK", -10,"OK"},
-		}
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ012345", 0, "ABCDEFGHIJKLMNOPQRSTUVWXYZ012345"},
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ012345", 10, "KLMNOPQRSTUVWXYZ012345"},
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ012345", 4, "EFGHIJKLMNOPQRSTUVWXYZ012345"},
+		{"OK", 3, ""},
+		{"OK", 1, "K"},
+		{"OK", 2, ""},	
+	}
 
 	for i, test := range tests {
-		var data = String{"Ok"}
-		actual := data.Substr(1)
+		var data = String{test.in}
+		actual := data.Substr(test.number)
 		if actual != test.expected {
-			t.Errorf("#%d: Substr(%v)=%s; expected %s", i, test.in, actual, test.expected)
+			t.Errorf("#%d: Substr(%v)=%s; expected %s", i, test.number, actual, test.expected)
+		}
+	}
+}
+
+func TestSubstrMulti(t *testing.T) {
+	tests := []struct {
+		in       string
+		number1  int
+		number2  int
+		expected string
+	}{
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ012345", 0, 6, "ABCDEF"},
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ012345", 10, 6, "KLMNOP"},
+	}
+
+	for i, test := range tests {
+		var data = String{test.in}
+		actual := data.Substr(test.number1, test.number2)
+		if actual != test.expected {
+			t.Errorf("#%d: Substr(%v,%v)=%s; expected %s", i, test.number1, test.number2, actual, test.expected)
 		}
 	}
 }
