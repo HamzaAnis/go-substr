ecab84d70c80f34b638e6d0da13d8a2af30514b5
hamzaanis <hamzaanis9514@gmail.com>
2019-02-02 12:47:54 +0500

    👓

--- substr.go
+++ substr.go
@@ -1,5 +1,14 @@
 package substr
 
-func Substr(n uint) string {
+import "fmt"
+
+// This struct holds the value.
+type String struct {
+	data string
+}
+
+// Substr clone of https://help.adobe.com/en_US/FlashPlatform/reference/actionscript/3/String.html#substr()
+func (v *String) Substr(n int) string {
+	fmt.Println(v.data)
 	return "OK"
 }
--- substr_test.go
+++ substr_test.go
@@ -5,17 +5,17 @@ import "testing"
 func TestSubstr(t *testing.T) {
 	tests := []struct {
 		in       string
+		number 	int
 		expected string
 	}{
-		{"OK", "OK"},
-		{"OK", "OK"},
-		{"OK", "OK"},
-		{"OK", "OK"},
-		{"OK", "OK"},
-	}
+		{"OK",0, "OK"},
+		{"OK", 10,"OK"},
+		{"OK", -10,"OK"},
+		}
 
 	for i, test := range tests {
-		actual := Substr(1)
+		var data = String{"Ok"}
+		actual := data.Substr(1)
 		if actual != test.expected {
 			t.Errorf("#%d: Substr(%v)=%s; expected %s", i, test.in, actual, test.expected)
 		}
